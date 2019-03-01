893fee5bc1c9f60ee7cbf809948d69c1a62e950c
Porter Savage <theportersavage@gmail.com>
2019-03-01 11:28:08 -0800

    add get clients test method and get clients method and created an instance of a new Clients class

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace HairSalon.Models
+{
+    public class Client
+    {
+        private int _id;
+        private string _name;
+        private int _stylist_id;
+
+        public Client(string name, int stylist_id, int id = 0)
+        {
+            _name = name;
+            _stylist_id = stylist_id;
+            _id = id;
+        }
+    }
+}
