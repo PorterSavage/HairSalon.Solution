44f21ebcb862e00d1ff7d8b2bc6f039a7f91738f
Porter Savage <theportersavage@gmail.com>
2019-03-01 10:12:19 -0800

    forgot to make first commit when setting up project structure, wa panicking about the project, i have also added 4 test methods that are all currently passing

---
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -0,0 +1,60 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+    [TestClass]
+    public class StylistTest : IDisposable
+    {
+        public StylistTest()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=porter_savage_test;";
+        }
+
+        public void Dispose()
+        {
+            Stylist.ClearAll();
+        }
+
+        [TestMethod]
+        public void StylistConstructer_CreateInstanceOfStylist_Stylist()
+        {
+            Stylist newStylist = new Stylist("test stylist");
+            Assert.AreEqual(typeof(Stylist), newStylist.GetType());
+        }
+
+        [TestMethod]
+        public void GetName_ReturnsName_String()
+        {
+            string name = "Test Stylist";
+            Stylist newStylist = new Stylist(name);
+            string result = newStylist.GetName();
+            Assert.AreEqual(name, result);
+        }
+
+        [TestMethod]
+        public void GetId_ReturnsId_Int()
+        {
+            string name = "Test Stylist";
+            Stylist newStylist = new Stylist(name);
+            int result = newStylist.GetId();
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void GetAll_ReturnsAllStylistObjects_StylistList()
+        {
+            string name1 = "Work";
+            string name2 = "School";
+            Stylist newStylist1 = new Stylist(name1);
+            newStylist1.Save();
+            Stylist newStylist2 = new Stylist(name2);
+            newStylist2.Save();
+            List<Stylist> newList = new List<Stylist> { newStylist1, newStylist2 };
+            List<Stylist> result = Stylist.GetAll();
+            CollectionAssert.AreEqual(newList, result);    
+        }
+    }
+}
---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace HairSalon.Models
+{
+    public class Stylist
+    {
+        private string _name;
+        private int _id;
+
+        public Stylist(string stylistName, int id = 0){
+            _name = stylistName;
+            _id = id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public static void ClearAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM stylists;";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public static List<Stylist> GetAll()
+        {
+            List<Stylist> allStylists = new List<Stylist> {};
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM stylists;";
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while (rdr.Read())
+            {
+                int stylistId = rdr.GetInt32(0);
+                string stylistName = rdr.GetString(1);
+                Stylist newStylist = new Stylist(stylistName, stylistId);
+                allStylists.Add(newStylist);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allStylists;
+        }
+    }
+}
