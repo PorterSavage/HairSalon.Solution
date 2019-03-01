bd52d27bb4e38f46cd23651be996fe51069acf04
Porter Savage <theportersavage@gmail.com>
2019-03-01 10:21:25 -0800

    Write logic to get my GetAll method to pass

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -59,5 +59,24 @@ namespace HairSalon.Models
             }
             return allStylists;
         }
+
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO stylists (name) VALUES (@name);";
+            MySqlParameter name = new MySqlParameter();
+            name.ParameterName = @"name";
+            name.Value = this._name;
+            cmd.Parameters.Add(name);
+            cmd.ExecuteNonQuery();
+            _id = (int)cmd.LastInsertedId;
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
     }
 }
