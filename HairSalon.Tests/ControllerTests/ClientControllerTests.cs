614b915a246dfc8a602025e185e032a7afc825f3
Porter Savage <theportersavage@gmail.com>
2019-03-01 16:01:46 -0800

    Add the rest of the controllers

---
+++ HairSalon.Tests/ControllerTests/ClientControllerTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using HairSalon.Controllers;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+    [TestClass]
+    public class ClientControllerTest
+    {
+        [TestMethod]
+        public void Create_ReturnsCorrectActionType_RedirectToActionResult()
+        {
+            ClientControllerTest controller = new ClientControllerTest();
+            ActionResult view = controller.Create("cut the hair");
+            Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
+        }
+    }
+}
