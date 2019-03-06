6b7862444b5eb4687657c35a886edce0df1c4d5d
Porter Savage <theportersavage@gmail.com>
2019-03-05 16:04:45 -0800

    Added Client controller test methods

--- HairSalon.Tests/ControllerTests/ClientControllerTests.cs
+++ HairSalon.Tests/ControllerTests/ClientControllerTests.cs
@@ -6,15 +6,71 @@ using HairSalon.Models;
 
 namespace HairSalon.Tests
 {
-    // [TestClass]
-    // public class ClientControllerTest
-    // {
-    //     [TestMethod]
-    //     public void Create_ReturnsCorrectActionType_RedirectToActionResult()
-    //     {
-    //         ClientControllerTest controller = new ClientControllerTest();
-    //         ActionResult view = controller.Create("cut the hair");
-    //         Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
-    //     }
-    // }
+    [TestClass]
+    public class ClientControllerTest
+    {
+        [TestMethod]
+        public void New_ReturnsCorrectView_True()
+        {
+            string name = "jen";
+            int stylistId = 0;
+            Stylist newStylist = new Stylist(name, stylistId);
+            newStylist.Save();
+            stylistId = newStylist.GetId();
+            ClientController controller = new ClientController();
+            ActionResult View = controller.New(stylistId);
+            Assert.IsInstanceOfType(View, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void New_HasCorrectModelType_Stylist()
+        {
+            string name = "jen";
+            int stylistId = 0;
+            Stylist newStylist = new Stylist(name, stylistId);
+            newStylist.Save();
+            stylistId = newStylist.GetId();
+            ClientController controller = new ClientController();
+            ViewResult view = controller.New(stylistId) as ViewResult;
+            var result = view.ViewData.Model;
+            Assert.IsInstanceOfType(result, typeof(Stylist));
+        }
+
+        [TestMethod]
+        public void Show_ReturnsCorrectView_True()
+        {
+            string stylistName = "jen";
+            int stylistId = 0;
+            string clientName = "bob";
+            int clientId = 0;
+            Stylist newStylist = new Stylist(stylistName, stylistId);
+            Client newClient = new Client(clientName, clientId);
+            newStylist.Save();
+            newClient.Save();
+            stylistId = newStylist.GetId();
+            clientId = newClient.GetId();
+            ClientController controller = new ClientController();
+            ActionResult view = controller.Show(stylistId, clientId);
+            Assert.IsInstanceOfType(view, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Show_HasCorrectModelType_Client()
+        {
+            string stylistName = "jen";
+            int stylistId = 0;
+            string clientName = "bob";
+            int clientId = 0;
+            Stylist newStylist = new Stylist(stylistName, stylistId);
+            Client newClient = new Client(clientName, clientId);
+            newStylist.Save();
+            newClient.Save();
+            stylistId = newStylist.GetId();
+            clientId = newClient.GetId();
+            ClientController controller = new ClientController();
+            ViewResult view = controller.Show(stylistId, clientId) as ViewResult;
+            var result = view.ViewData.Model;
+            Assert.IsInstanceOfType(result, typeof(Dictionary<string, object>));
+        }
+    }
 }
