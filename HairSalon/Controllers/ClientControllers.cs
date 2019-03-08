f20b6e0f69fc096d8c8cb066986d37a59e462f21
Porter Savage <theportersavage@gmail.com>
2019-03-08 12:46:49 -0800

    stuck on trying to figure out how to view an individual client from the client list

--- HairSalon/Controllers/ClientControllers.cs
+++ HairSalon/Controllers/ClientControllers.cs
@@ -6,6 +6,22 @@ namespace HairSalon.Controllers
 {
     public class ClientController : Controller
     {
+        [HttpGet("/clients")]
+        public ActionResult Index()
+        {
+            List<Client> allClients = Client.GetAll();
+            return View(allClients);
+        }
+
+        [HttpGet("clients/{clientId}")]
+        public ActionResult Show(int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Client selectedClient = Client.Find(id);
+            model.Add("client", selectedClient);
+            return View(model);
+        }
+
         [HttpGet("/stylists/{stylistId}/clients/new")]
         public ActionResult New(int stylistId)
         {
@@ -22,6 +38,24 @@ namespace HairSalon.Controllers
             model.Add("client", client);
             model.Add("stylist", stylist);
             return View(model);
+        }
+
+        [HttpGet("/stylists/{stylistId}/clients/{clientId}/delete")]
+        public ActionResult Delete(int stylistId, int clientId)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Client client = Client.Find(clientId);
+            client.Delete();
+            model.Add("client", client);
+            return View(model);
+        }
+
+        [HttpPost("/clients/deleted")]
+        public ActionResult DeleteClient(int clientId)
+        {
+            Client client = Client.Find(clientId);
+            client.Delete();
+            return RedirectToAction("Index");
         }
     }
 }
